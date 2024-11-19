206eaeee748340f07e9e1410f5f390888977ad59
Teto-Novala <tetocoding@gmail.com>
2024-11-20 00:40:58 +0700

    feat: add register desktop

--- src/views/register/RegisterView.vue
+++ src/views/register/RegisterView.vue
@@ -59,6 +59,133 @@
       </form>
     </div>
     <!-- mobile end -->
+    <!-- tablet start -->
+    <div class="hidden xl:hidden md:flex md:flex-col md:gap-y-9">
+      <img
+        src="/src/assets/images/register/register.jpg"
+        alt="register"
+        class="w-full rounded-lg h-[20rem] object-cover object-top"
+      />
+      <div>
+        <h1 class="font-volkhov text-5xl text-center">ASPA</h1>
+        <form
+          @submit.prevent="createHandler"
+          class="flex flex-col gap-y-8"
+        >
+          <div class="flex flex-col gap-y-7">
+            <Input
+              placeholder="First Name"
+              v-model:model="data.firstname"
+            />
+            <Input
+              placeholder="Last Name"
+              v-model:model="data.lastname"
+            />
+            <Input
+              placeholder="Email Address"
+              type="email"
+              v-model:model="data.email"
+            />
+            <Input
+              placeholder="Phone Number (+62xxx)"
+              v-model:model="data.phone"
+            />
+            <Input
+              placeholder="Password"
+              type="password"
+              v-model:model="data.password"
+            />
+            <Input
+              placeholder="Confirm Password"
+              type="password"
+              v-model:model="data.confirmPassword"
+            />
+          </div>
+          <div>
+            <Button
+              type="submit"
+              class="w-full"
+              >Create Account</Button
+            >
+            <p class="font-poppins text-right mt-4">
+              Already have an account?
+              <span
+                ><RouterLink
+                  class="text-secondary transition-all hover:underline"
+                  to="/signin"
+                  >Login</RouterLink
+                ></span
+              >
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+    <!-- tablet end -->
+    <!-- desktop start -->
+    <div class="hidden xl:flex xl:h-screen xl:items-center xl:gap-x-16">
+      <img
+        src="/src/assets/images/register/register.jpg"
+        alt="register"
+        class="w-1/2 h-5/6 object-cover object-center rounded-lg"
+      />
+      <div class="w-1/2 flex flex-col gap-y-8">
+        <h1 class="font-volkhov text-3xl">ASPA</h1>
+        <h2 class="font-volkhov">Create Account</h2>
+        <form
+          @submit.prevent="createHandler"
+          class="flex flex-col gap-y-4"
+        >
+          <div class="grid grid-cols-2 gap-x-4 gap-y-3">
+            <Input
+              placeholder="First Name"
+              v-model:model="data.firstname"
+            />
+            <Input
+              placeholder="Last Name"
+              v-model:model="data.lastname"
+            />
+            <Input
+              placeholder="Email Address"
+              type="email"
+              v-model:model="data.email"
+            />
+            <Input
+              placeholder="Phone Number (+62xxx)"
+              v-model:model="data.phone"
+            />
+            <Input
+              placeholder="Password"
+              type="password"
+              v-model:model="data.password"
+            />
+            <Input
+              placeholder="Confirm Password"
+              type="password"
+              v-model:model="data.confirmPassword"
+            />
+          </div>
+          <div class="flex flex-col gap-y-3">
+            <Button
+              type="submit"
+              class="w-full"
+              >Create Account</Button
+            >
+            <p class="font-poppins self-end">
+              Already have an account?
+              <span
+                ><RouterLink
+                  class="text-secondary transition-all hover:underline"
+                  to="/signin"
+                  >Login</RouterLink
+                ></span
+              >
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+    <!-- desktop end -->
   </section>
 </template>
 
@@ -75,11 +202,12 @@ import {
 } from "@vuelidate/validators";
 import axios from "axios";
 import { computed, reactive } from "vue";
-import { RouterLink } from "vue-router";
+import { RouterLink, useRouter } from "vue-router";
 import { useToast } from "vue-toastification";
 import { phoneValidate } from "@/validations/phone";
 
 const toast = useToast();
+const router = useRouter();
 
 const data = reactive({
   firstname: "",
@@ -128,7 +256,9 @@ const createHandler = async () => {
         "http://localhost:5000/api/user/register",
         { firstname, lastname, email, phone, password }
       );
-      toast.success("Berhasil membuat akun");
+      toast.success("Berhasil membuat akun", {
+        onClose: () => router.push("/signin"),
+      });
     } catch (error) {
       console.error(error);
       toast.error(error.response.data.message);
