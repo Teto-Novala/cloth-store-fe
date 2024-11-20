8c07b0806cb374f57931a187796c2e47acb7039a
Teto-Novala <tetocoding@gmail.com>
2024-11-21 05:32:05 +0700

    feat: add forgot password desktop

--- src/views/forgot-password/ForgotPasswordView.vue
+++ src/views/forgot-password/ForgotPasswordView.vue
@@ -98,13 +98,64 @@
     </div>
     <!-- tablet end -->
 
+    <!-- desktop start -->
+    <div class="hidden xl:flex xl:items-center xl:h-screen gap-x-16">
+      <img
+        src="/src/assets/images/forgot-password/forgot-password.jpg"
+        alt="forgot password"
+        class="w-1/2 h-3/4 object-cover"
+      />
+      <div class="w-1/2 flex flex-col justify-between gap-y-12">
+        <h1 class="font-volkhov text-3xl">ASPA</h1>
+        <h2 class="font-volkhov text-lg">Forgot Password</h2>
+        <form
+          @submit.prevent="submitHandler"
+          class="grid grid-cols-2 gap-x-4 gap-y-7"
+        >
+          <Input
+            placeholder="Firstname"
+            v-model:model="form.firstname"
+          />
+          <Input
+            placeholder="Lastname"
+            v-model:model="form.lastname"
+          />
+          <Input
+            placeholder="Email"
+            type="email"
+            v-model:model="form.email"
+          />
+          <Input
+            placeholder="Phone"
+            v-model:model="form.phone"
+          />
+          <Button
+            type="submit"
+            class="w-full col-span-2"
+            >Send Confirmation Code</Button
+          >
+          <p class="font-poppins text-right col-span-2">
+            Already have an account?
+            <span
+              ><RouterLink
+                class="text-secondary transition-all hover:underline"
+                to="/signin"
+                >Login</RouterLink
+              ></span
+            >
+          </p>
+        </form>
+      </div>
+    </div>
+    <!-- desktop end -->
+
     <!-- pop up start -->
     <div
       class="w-full h-screen bg-black/50 fixed z-50 top-0 left-0 px-8 flex items-center"
       v-if="isConfirm"
     >
       <div
-        class="bg-white w-full h-fit p-4 rounded-lg flex flex-col justify-center text-center gap-y-4 font-poppins"
+        class="bg-white w-full xl:w-fit xl:mx-auto h-fit p-4 rounded-lg flex flex-col justify-center text-center gap-y-4 font-poppins"
       >
         <p class="text-xl">Your confirmation code is</p>
         <p class="text-2xl">{{ store.data.confirmationCode }}</p>
