99f94689979151d022deeec359ae85fd73e8b181
Teto-Novala <tetocoding@gmail.com>
2024-11-21 09:35:09 +0700

    feat: add confirmation code mobile

--- src/views/confirmation-code/ConfirmationCodeView.vue
+++ src/views/confirmation-code/ConfirmationCodeView.vue
@@ -1,3 +1,28 @@
-<template>COnfirmaton code</template>
+<template>
+  <section class="px-8">
+    <!-- mobile start -->
+    <div class="md:hidden flex flex-col gap-y-5 items-center">
+      <img
+        src="/src/assets/images/forgot-password/forgot-password.jpg"
+        alt="confirmation code"
+        class="rounded-lg"
+      />
+      <h1 class="font-volkhov text-3xl">ASPA</h1>
+      <h2 class="self-start font-volkhov">Enter The Confirmation Code</h2>
+      <form class="flex flex-col gap-y-4 w-full">
+        <Input placeholder="Confirmation Code" />
+        <Button
+          type="submit"
+          class="w-full"
+          >Recover Account</Button
+        >
+      </form>
+    </div>
+    <!-- mobile end -->
+  </section>
+</template>
 
-<script setup></script>
+<script setup>
+import Button from "@/components/Button.vue";
+import Input from "@/components/Input.vue";
+</script>
